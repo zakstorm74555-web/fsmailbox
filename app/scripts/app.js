1c77222eb5e478072a4634b732978cd25377a189
Deadly Ghost <zakstorm74555@gmail.com>
2026-05-22 10:30:26 +0500

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,8 @@
 /* global angular, io, location */
 
 /**
- * App Config
+ * App Config — FsMail Edition
+ * Author: FatahShaheen OS (Paradox Studio)
  */
 
 const app = angular.module('mailDevApp', ['ngRoute', 'ngResource', 'ngSanitize', 'ngCookies'])
@@ -14,28 +15,29 @@ app.config(['$routeProvider', function ($routeProvider) {
 }])
 
 app.run(['$rootScope', function ($rootScope) {
-  // Connect Socket.io
+  // Connect Socket.io Network Stream
   const socket = io({
     path: location.pathname + 'socket.io'
   })
 
   socket.on('newMail', function (data) {
+    // Dynamic Inward Trigger Handshake
     $rootScope.$emit('newMail', data)
   })
 
   socket.on('deleteMail', function (data) {
+    // Structural Storage Cleanup Handshake
     $rootScope.$emit('deleteMail', data)
   })
 
   $rootScope.$on('Refresh', function () {
-    console.log('Refresh event called.')
+    console.log('[FsMail Core] Storage layer sync refresh triggered.')
   })
 }])
 
 /**
- * filter to encode special HTML characters as HTML entities
+ * Filter to encode special HTML characters safely
  */
-
 app.filter('escapeHTML', function () {
   return function (text) {
     if (text) {
@@ -51,9 +53,8 @@ app.filter('escapeHTML', function () {
 })
 
 /**
- * filter to encode URI
+ * Filter to encode URI parameters
  */
-
 app.filter('encodeURIComponent', function ($window) {
   return $window.encodeURIComponent
 })
